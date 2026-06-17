5608f7ad4c520e4b2e32dff1f38ca33b7e2e29ef
HealingArty <sadwar1001@gmail.com>
2026-06-17 19:33:51 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,2 +1,11 @@
-from .updated_prompt_randomizer import PromptRandomizerV10
-NODE_CLASS_MAPPINGS = {"PromptRandomizerV10": PromptRandomizerV10}
+from .updated_prompt_randomizer import HEARINGARTY_PromptRandomizerV10
+
+NODE_CLASS_MAPPINGS = {
+    "HEARINGARTY_PromptRandomizerV10": HEARINGARTY_PromptRandomizerV10
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "HEARINGARTY_PromptRandomizerV10": "HEARINGARTY Prompt Randomizer V10"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
